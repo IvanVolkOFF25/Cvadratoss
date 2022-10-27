afb0356b7109c333f3c7c1ac397535554778f0de
IvanVolkOFF25 <ivan.s.volkov@vsegda-da.com>
2022-10-27 23:23:28 +0300

    Initial commit

---
+++ src/main/java/Cvadratoss.java
@@ -0,0 +1,10 @@
+public class Cvadratoss {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 7;
+        int output = a * b;
+        System.out.println("Площадь квадрата равна: " + output);
+
+
+    }
+}
---
+++ src/main/java/Treugolnik.java
@@ -0,0 +1,8 @@
+public class Treugolnik {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        int output = a * b * 1/2;
+        System.out.println("Площадь треугольника равна: " + output);
+    }
+}
